01a4a836e7db6c95bc7abc2dfbec00d56619a158
jaop-ucl <85939245+jaop-ucl@users.noreply.github.com>
2021-06-15 13:38:49 +0100

    Add files via upload

---
+++ js/default.js
@@ -0,0 +1,8 @@
+function navigation(i){
+	document.getElementById('poordesign').style.display='none';
+	document.getElementById('methods').style.display='none';
+	document.getElementById('design').style.display='none';
+	document.getElementById('test').style.display='none';
+	document.getElementById('analysis').style.display='none';
+	document.getElementById(i).style.display='block';
+}
