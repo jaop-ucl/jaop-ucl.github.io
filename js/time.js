3b319a173acd807d93882a7b328ff9c9f09f76ab
jaop-ucl <85939245+jaop-ucl@users.noreply.github.com>
2022-04-08 10:45:18 +0100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -8,24 +8,24 @@ const now = DateTime
 // Get the hour from today
 const hour = DateTime.hour
 // Add 7 days to now
-const weekAdd = DateTime.plus({days: 7});
+//const weekAdd = DateTime.plus({days: 7});
 
 // Now formatted like "Monday, November 23, 5pm"
-const dateFormatted = DateTime.toFormat("EEEE',' MMMM d',' ha")
+//const dateFormatted = DateTime.toFormat("EEEE',' MMMM d',' ha")
 
 // Date and time on Lord Howe Island
-const dateTimeLocale = DateTime.setZone('Australia/Lord_Howe')
+//const dateTimeLocale = DateTime.setZone('Australia/Lord_Howe')
 
 //Parsing
-const dateTimeParsing1 = DateTime.fromFormat('Jul 8, 2005', 'MMM d, y').toJSDate()
-const dateTimeParsing2 = DateTime.fromFormat('2005-07-08', 'y-MM-dd').toJSDate()
+//const dateTimeParsing1 = DateTime.fromFormat('Jul 8, 2005', 'MMM d, y').toJSDate()
+//const dateTimeParsing2 = DateTime.fromFormat('2005-07-08', 'y-MM-dd').toJSDate()
 
-document.getElementById("package-link").innerHTML = "Luxon"
-document.getElementById("package-link").href = "https://www.skypack.dev/view/luxon"
+//document.getElementById("package-link").innerHTML = "Luxon"
+//document.getElementById("package-link").href = "https://www.skypack.dev/view/luxon"
 document.getElementById('today').innerHTML = now
 document.getElementById('today-hour').innerHTML = hour
-document.getElementById('week-add').innerHTML = weekAdd
-document.getElementById('date-formatted').innerHTML = dateFormatted
-document.getElementById('date-time-locale').innerHTML = dateTimeLocale
-document.getElementById("date-time-parsing-1").innerHTML = dateTimeParsing1;
-document.getElementById("date-time-parsing-2").innerHTML = dateTimeParsing2;
+//document.getElementById('week-add').innerHTML = weekAdd
+//document.getElementById('date-formatted').innerHTML = dateFormatted
+//document.getElementById('date-time-locale').innerHTML = dateTimeLocale
+//document.getElementById("date-time-parsing-1").innerHTML = dateTimeParsing1;
+//document.getElementById("date-time-parsing-2").innerHTML = dateTimeParsing2;
