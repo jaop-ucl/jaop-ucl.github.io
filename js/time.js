ef43f853d6db75a2b602039d3d93eeb33d2df7d2
jaop-ucl <85939245+jaop-ucl@users.noreply.github.com>
2022-04-08 14:19:40 +0100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -5,26 +5,31 @@
 
 var DateTime = luxon.DateTime
 
-const currenttime = DateTime.now().toLocal().toLocaleString(DateTime.DATETIME_FULL)
-const conferenceutc = DateTime.utc(2022, 4, 27, 13, 00).toLocaleString(DateTime.DATETIME_FULL)
-const conference = DateTime.utc(2022, 4, 27, 13, 00).toLocal().toLocaleString(DateTime.DATETIME_FULL)
+function updateClock() {
+    const currenttime = DateTime.now().toLocal().toLocaleString(DateTime.DATETIME_FULL)
+    const conferenceutc = DateTime.utc(2022, 4, 27, 13, 00).toLocaleString(DateTime.DATETIME_FULL)
+    const conference = DateTime.utc(2022, 4, 27, 13, 00).toLocal().toLocaleString(DateTime.DATETIME_FULL)
 
-const date1 = DateTime.utc(2022, 4, 27, 13, 00)
+    const date1 = DateTime.utc(2022, 4, 27, 13, 00)
 
-const diffintime = date1.diff(DateTime.now(), ["years", "months", "days", "hours", "minutes"])
+    const diffintime = date1.diff(DateTime.now(), ["years", "months", "days", "hours", "minutes", "seconds"])
 
-const days = Math.floor(diffintime.days)
-const hours = Math.floor(diffintime.hours)
-const minutes = Math.floor(diffintime.minutes)
+    const days = Math.floor(diffintime.days)
+    const hours = Math.floor(diffintime.hours)
+    const minutes = Math.floor(diffintime.minutes)
+    const seconds = Math.floor(diffintime.seconds)
 
-const timeuntilconoference = 'Days:' + days + ' Hours:' + hours + ' Minutes:' + minutes
+    const timeuntilconoference = 'Days:' + days + ' Hours:' + hours + ' Minutes:' + minutes + ' Seconds:' + seconds
 
-document.getElementById('today').innerHTML = currenttime
-document.getElementById('conference-time-utc').innerHTML = conferenceutc
-document.getElementById('conference-time').innerHTML = conference
-document.getElementById('conference-time-until').innerHTML = timeuntilconoference
+    document.getElementById('today').innerHTML = currenttime
+    document.getElementById('conference-time-utc').innerHTML = conferenceutc
+    document.getElementById('conference-time').innerHTML = conference
+    document.getElementById('conference-time-until').innerHTML = timeuntilconoference
+}
 
-
-const version = 7
+const version = 8
 
 document.getElementById('version').innerHTML = version
+
+updateClock();
+setInterval(updateClock, 1000);
