7642eba46f07c25f2efde77865e5916cc5332b66
jaop-ucl <85939245+jaop-ucl@users.noreply.github.com>
2022-04-08 12:31:21 +0100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -10,7 +10,10 @@ const now = DateTime.now().toUTC().toLocaleString(DateTime.DATETIME_FULL)
 const conference = DateTime.fromObject({year: 2022, month: 4, day: 27, hour: 14, minute: 05}, { zone: 'Europe/London' }).toLocaleString(DateTime.DATETIME_FULL)
 const conferenceutc = DateTime.fromObject({year: 2022, month: 4, day: 27, hour: 14, minute: 05}).toLocaleString(DateTime.DATETIME_FULL)
 
-const timeuntilconoference = now.diff(conferenceutc, ["years", "months", "days", "hours"])
+const date1 = luxon.DateTime.fromISO("2020-09-06T12:00")
+const date2 = luxon.DateTime.fromISO("2019-06-10T14:00")
+
+const timeuntilconoference = date1.diff(date2, ["years", "months", "days", "hours"])
 
 document.getElementById('today').innerHTML = now
 document.getElementById('conference-time').innerHTML = conference
