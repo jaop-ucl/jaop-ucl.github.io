85a72f814cca729821556ffe7378c03df670def0
jaop-ucl <85939245+jaop-ucl@users.noreply.github.com>
2022-04-08 12:48:58 +0100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -7,17 +7,18 @@
 var DateTime = luxon.DateTime
 
 const now = DateTime.now().toUTC().toLocaleString(DateTime.DATETIME_FULL)
-const conference = DateTime.fromObject({year: 2022, month: 4, day: 27, hour: 14, minute: 05}, { zone: 'Europe/London' }).toLocaleString(DateTime.DATETIME_FULL)
-const conferenceutc = DateTime.fromObject({year: 2022, month: 4, day: 27, hour: 14, minute: 05}).toLocaleString(DateTime.DATETIME_FULL)
+const conferenceutc = DateTime.utc(2022, 4, 27, 14, 00).toLocaleString(DateTime.DATETIME_FULL)
+const conference = DateTime.utc(2022, 4, 27, 14, 00).toLocal().toLocaleString(DateTime.DATETIME_FULL)
+
 
 const date1 = luxon.DateTime.fromISO("2020-09-06T12:00")
 const date2 = luxon.DateTime.fromISO("2019-06-10T14:00")
 
+DateTime.utc(2017, 5, 15, 17, 36).toLocal()
+
 const timeuntilconoference = date1.diff(date2, ["years", "months", "days", "hours"]).toFormat("HH 'hours and' mm 'minutes'")
 
-console.log(timeuntilconoference.toObject())
-
 document.getElementById('today').innerHTML = now
-document.getElementById('conference-time').innerHTML = conference
 document.getElementById('conference-time-utc').innerHTML = conferenceutc
+document.getElementById('conference-time').innerHTML = conference
 document.getElementById('conference-time-until').innerHTML = timeuntilconoference
