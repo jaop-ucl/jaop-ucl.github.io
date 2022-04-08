49f14dae8fa07e9412d866adc56347de0d047c19
jaop-ucl <85939245+jaop-ucl@users.noreply.github.com>
2022-04-08 14:01:03 +0100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -3,16 +3,13 @@
 
 // examples : https://moment.github.io/luxon/demo/global.html
 
-
 var DateTime = luxon.DateTime
 
 const currenttime = DateTime.now().toLocal().toLocaleString(DateTime.DATETIME_FULL)
 const conferenceutc = DateTime.utc(2022, 4, 27, 13, 00).toLocaleString(DateTime.DATETIME_FULL)
 const conference = DateTime.utc(2022, 4, 27, 13, 00).toLocal().toLocaleString(DateTime.DATETIME_FULL)
 
-
-//const date1 = luxon.DateTime.fromISO("2023-04-27T13:00")
-const date1 = luxon.DateTime.utc(2022, 4, 27, 13, 00)
+const date1 = DateTime.utc(2022, 4, 27, 13, 00)
 
 const diffintime = date1.diff(DateTime.now(), ["years", "months", "days", "hours", "minutes"])
 
@@ -20,7 +17,7 @@ const days = Math.floor(diffintime.days)
 const hours = Math.floor(diffintime.hours)
 const minutes = Math.floor(diffintime.minutes)
 
-const timeuntilconoference = 'Days:' + days + ' Hours:' + hours + 'Minutes:' + minutes
+const timeuntilconoference = 'Days:' + days + ' Hours:' + hours + ' Minutes:' + minutes
 
 document.getElementById('today').innerHTML = currenttime
 document.getElementById('conference-time-utc').innerHTML = conferenceutc
