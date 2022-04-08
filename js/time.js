990f25725fbe14764e1b4955a06aec4f9dc27cde
jaop-ucl <85939245+jaop-ucl@users.noreply.github.com>
2022-04-08 13:57:49 +0100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -11,14 +11,16 @@ const conferenceutc = DateTime.utc(2022, 4, 27, 13, 00).toLocaleString(DateTime.
 const conference = DateTime.utc(2022, 4, 27, 13, 00).toLocal().toLocaleString(DateTime.DATETIME_FULL)
 
 
-const date1 = luxon.DateTime.fromISO("2023-04-27T13:00")
+//const date1 = luxon.DateTime.fromISO("2023-04-27T13:00")
+const date1 = luxon.DateTime.utc(2022, 4, 27, 13, 00)
 
-const diffintime = date1.diff(DateTime.now(), ["years", "months", "days", "hours"])
+const diffintime = date1.diff(DateTime.now(), ["years", "months", "days", "hours", "minutes"])
 
 const days = Math.floor(diffintime.days)
 const hours = Math.floor(diffintime.hours)
+const minutes = Math.floor(diffintime.minutes)
 
-const timeuntilconoference = 'Days:' + days + ' Hours:' + hours
+const timeuntilconoference = 'Days:' + days + ' Hours:' + hours + 'Minutes:' + minutes
 
 document.getElementById('today').innerHTML = currenttime
 document.getElementById('conference-time-utc').innerHTML = conferenceutc
@@ -26,6 +28,6 @@ document.getElementById('conference-time').innerHTML = conference
 document.getElementById('conference-time-until').innerHTML = timeuntilconoference
 
 
-const version = 6
+const version = 7
 
 document.getElementById('version').innerHTML = version
