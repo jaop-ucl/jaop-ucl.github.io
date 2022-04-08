2b5e0c054b4c2baf51293ebb20778acff6643b12
jaop-ucl <85939245+jaop-ucl@users.noreply.github.com>
2022-04-08 12:13:21 +0100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -13,7 +13,7 @@ const hour = DateTime.now().hour
 // Now formatted like "Monday, November 23, 5pm"
 //const dateFormatted = DateTime.toFormat("EEEE',' MMMM d',' ha")
 const conference = DateTime.fromObject({year: 2022, month: 4, day: 27, hour: 14, minute: 05}, { zone: 'Europe/London' }).toFormat("EEEE',' MMMM d',' ha")
-const conferenceutc = DateTime.fromObject({year: 2022, month: 4, day: 27, hour: 14, minute: 05})..toLocaleString(DateTime.DATETIME_FULL)
+const conferenceutc = DateTime.fromObject({year: 2022, month: 4, day: 27, hour: 14, minute: 05}).toLocaleString(DateTime.DATETIME_FULL)
 
 // Date and time on Lord Howe Island
 //const dateTimeLocale = DateTime.setZone('Australia/Lord_Howe')
