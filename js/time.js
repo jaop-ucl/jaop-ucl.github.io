95c40527fd94ff3d4d7f7c203941890f872b2f6e
jaop-ucl <85939245+jaop-ucl@users.noreply.github.com>
2022-04-08 12:40:48 +0100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -15,7 +15,7 @@ const date2 = luxon.DateTime.fromISO("2019-06-10T14:00")
 
 const timeuntilconoference = date1.diff(date2, ["years", "months", "days", "hours"]).toFormat("HH 'hours and' mm 'minutes'")
 
-console.log(diff.toObject())
+console.log(timeuntilconoference.toObject())
 
 document.getElementById('today').innerHTML = now
 document.getElementById('conference-time').innerHTML = conference
